9d19eb4f72700502c909c0b483b7e8c33704934e
Emine Ildes Egri <93899335+eildes06@users.noreply.github.com>
2022-12-12 14:54:16 +0300

    Update Forecasting_streamlit.py

--- Forecasting_streamlit.py
+++ Forecasting_streamlit.py
@@ -11,29 +11,30 @@ dtf = pd.read_excel('Veri-Seti.xlsx',
                  index_col='Date',
                  parse_dates=True
                 )
-image = Image.open('leave.jpg')
+image = Image.open('car.jpg')
 html_temp = """
 <div style="background-color:Blue;padding:10px">
 <h2 style="color:white;text-align:center;">Employee Churn Prediction - GROUP-9 </h2>
 </div><br>"""
-st.set_page_config(page_title='Employee Churn Prediction', page_icon='👩‍💻')
+st.set_page_config(page_title='Car Price Prediction', page_icon='👩‍💻')
 st.image(image)
 st.markdown('***')
-st.markdown('# <center><span style="color:#286608">Employee Churn Prediction</span></center>',unsafe_allow_html=True )
-st.markdown("#### <center>Use the sidebar to enter your Employee's specifications.</center>",unsafe_allow_html=True)
+st.markdown('# <center><span style="color:#286608">Car Price Prediction</span></center>',unsafe_allow_html=True )
+st.markdown("#### <center>Use the sidebar to enter your cost's specifications.</center>",unsafe_allow_html=True)
 st.markdown('***')
+					    
 
-satisfaction_level = st.sidebar.slider(label="Satisfaction Level", min_value=0.0, max_value=1.0, step=0.01)
-last_evaluation = st.sidebar.slider(label="Last Evaluation", min_value=0.0, max_value=1.0, step=0.01)
-number_project = st.sidebar.slider(label="Number Of Project", min_value=0, max_value=10, step=1)
-average_montly_hours = st.sidebar.slider(label="Average Monthly Hours", min_value=0, max_value=350, step=10)
-time_spend_company = st.sidebar.slider("Time Spend in Company", min_value=1, max_value=10, step=1)
+OTV Orani = st.sidebar.slider(label="OTV Orani", min_value=0.0, max_value=1.0, step=0.01)
+Faiz = st.sidebar.slider(label="Faiz", min_value=0.0, max_value=1.0, step=0.01)
+EUR/TL = st.sidebar.slider(label="EUR/TL", min_value=0, max_value=10, step=1)
+Kredi Stok = st.sidebar.slider(label="Kredi Stok", min_value=0, max_value=350, step=10)
 
-churn = pd.DataFrame({"satisfaction_level" : [satisfaction_level],
-                    "last_evaluation" : [last_evaluation],
-                    "number_project" : [number_project],
-                    "average_montly_hours" : [average_montly_hours],
-                    "time_spend_company" : [time_spend_company],
+
+churn = pd.DataFrame({"OTV Orani" : [OTV Orani],
+                    "Faiz" : [Faiz],
+                    "EUR/TL" : [EUR/TL],
+                    "Kredi Stok" : [Kredi Stok]
+               
                     })
 
 hide_table_row_index = """
@@ -44,12 +45,12 @@ hide_table_row_index = """
             """        
 st.markdown(hide_table_row_index, unsafe_allow_html=True)   
 
-showdf = churn.rename(columns={'satisfaction_level': 'Satisfaction Level',
-                             'last_evaluation': 'Last Evaluation',
-                             'number_project': 'Number Of Project',
-                             'average_montly_hours': 'Average Monthly Hours',
-                             'time_spend_company': 'Time Spend in Company'})
-st.markdown("#### <center>Employee Information</center>",unsafe_allow_html=True)
+showdf = churn.rename(columns={'OTV Orani': 'OTV Orani',
+                             'Faiz': 'Faiz',
+                             'EUR/TL': 'EUR/TL',
+                             'Kredi Stok': 'Kredi Stok'
+                          })
+st.markdown("#### <center>Cost Information</center>",unsafe_allow_html=True)
 st.table(showdf)
 
 prediction = model.predict(churn)
@@ -58,12 +59,10 @@ prediction = model.predict(churn)
 st.subheader('Click PREDICT if configuration is OK')
 
 if st.button('PREDICT'):
-	if prediction[0]==0:
-		st.success(prediction[0])
-		st.success(f'Employee will STAY :)')
-	elif prediction[0]==1:
-		st.warning(prediction[0])
-		st.warning(f'Employee will LEAVE :(')
+	
+		st.success(prediction.mean())
+		st.success(f'Cost will BE :)')
+	
 
 
 hide_streamlit_style = """
